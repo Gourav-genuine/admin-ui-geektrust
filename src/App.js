3934a6ba840da21b6b30fef6cace0cdbe40a264b
Gourav <36379916+Gourav-genuine@users.noreply.github.com>
2021-09-11 23:40:35 +0530

    Refined

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import MaterialTable from "material-table";
 
-function App() {
+const App = () => {
+  const baseUrl =
+    "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+  const [data, setData] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const getData = () => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((resJson) => {
+        setData(resJson);
+        return resJson;
+      });
+  };
+
+  const columns = [
+    { title: "Name", field: "name" },
+    { title: "Email", field: "email" },
+    { title: "Role", field: "role" },
+  ];
+
+  const deleteSelected = () => {
+    const updatedData = data.filter((row) => !selectedRows.includes(row));
+    setData(updatedData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div></div>
+        <div className="d-flex justify-content-center">
+          <h4>Admin - Panel</h4>
+          {console.log(data)}
+        </div>
+        <div>
+          <MaterialTable
+            columns={columns}
+            data={data}
+            title="User Information"
+            onSelectionChange={(rows) => {
+              setSelectedRows(rows);
+            }}
+            editable={{
+              onRowAdd: (newRow) =>
+                new Promise((resolve, reject) => {
+                  setData([...data, newRow]);
+                  resolve();
+                }),
+              onRowUpdate: (newRow, oldRow) =>
+                new Promise((resolve, reject) => {
+                  const updatedData = [...data];
+                  updatedData[oldRow.tableData.id] = newRow;
+                  setData(updatedData);
+                  resolve();
+                }),
+              onRowDelete: (selectedRow) =>
+                new Promise((resolve, reject) => {
+                  console.log(selectedRow);
+                  const updatedData = [...data];
+                  updatedData.splice(selectedRow.tableData.id, 1);
+                  setData(updatedData);
+                  resolve();
+                }),
+            }}
+            options={{
+              pageSize: 10,
+              paginationType: "stepped",
+              exportButton: true,
+              addRowPosition: "first",
+              actionsColumnIndex: -1,
+              selection: true,
+
+              grouping: true,
+              columnsButton: true,
+            }}
+            actions={[
+              {
+                icon: "delete",
+                tooltip: "Delete all selected rows",
+                onClick: () => deleteSelected(),
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
